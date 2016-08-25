991ed81d414c3f6755703dbe24541f0bcfb5f4c6
Fan Zhang <bl4ck5unxx@gmail.com>
2016-08-25 14:02:57 -0400

    update

--- base.py
+++ base.py
@@ -46,11 +46,10 @@ class Engine (object):
 
     def write_utf8(self, utf8, filename):
         try:
-            # out = utf8.encode('utf-8')
-            out = utf8.encode('ascii', 'xmlcharrefreplace')
+            out = utf8.encode('utf-8')
+            # out = utf8.encode('ascii', 'xmlcharrefreplace')
         except:
-            print 'Already encoded'
-            out = utf8
+            raise
 
         self.write(out, filename)
 
