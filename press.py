41c71fc7b5fde2685a335820109f6c79bb81e169
Fan Zhang <fanz@linux.com>
2016-12-08 19:57:43 -0500

    replace strptime with dateutil.parser

--- press.py
+++ press.py
@@ -1,4 +1,6 @@
 import time
+from dateutil import parser
+import traceback
 
 class Press (object):
     def __init__(self, title, url, venue, date):
@@ -18,15 +20,19 @@ import codecs
 
 def get_all_press():
     press_all = []
-
     with codecs.open('content/press/pressroll-all.csv', 'r', encoding='utf-8') as infile:
         for line in reversed(infile.readlines()):
-            comps = line.split(';')
-            url = comps[0].strip('"')
-            venue = comps[1].strip('"')
-            date = time.strptime(comps[2], "%m/%d/%y")
-            title = comps[3].strip().strip('"')
-            press_all.append(Press(title, url, venue, date))
+            try:
+                comps = line.split(';')
+                url = comps[0].strip('"')
+                venue = comps[1].strip('"')
+                # date = time.strptime(comps[2], "%m/%d/%y")
+                date = parser.parse(comps[2]).timetuple()
+                title = comps[3].strip().strip('"')
+                press_all.append(Press(title, url, venue, date))
+            except Exception as e:
+                print 'Error in processing %s...' % line[:100]
+                traceback.print_exc()
 
     return sorted(press_all, key=lambda x: x.date, reverse=True)
 
@@ -34,11 +40,15 @@ def get_featured_press():
     press_featured = []
     with codecs.open('content/press/pressroll-featured.csv', 'r', encoding='utf-8') as infile:
         for line in reversed(infile.readlines()):
-            comps = line.split(';')
-            url = comps[0].strip('"')
-            venue = comps[1].strip('"')
-            date = time.strptime(comps[2], "%m/%d/%y")
-            title = comps[3].strip().strip('"')
-            press_featured.append(Press(title, url, venue, date))
+            try:
+                comps = line.split(';')
+                url = comps[0].strip('"')
+                venue = comps[1].strip('"')
+                date = parser.parse(comps[2]).timetuple()
+                title = comps[3].strip().strip('"')
+                press_featured.append(Press(title, url, venue, date))
+            except Exception as e:
+                print 'Error in processing %s...' % line[:100]
+                traceback.print_exc()
 
     return sorted(press_featured, key=lambda x: x.date, reverse=True)
