aa5c5b219bfd662b0f7b2ee5f8d53f2719d65ee9
Fan Zhang <bl4ck5unxx@gmail.com>
2019-01-28 12:33:31 -0500

    improve compression script

--- compress.sh
+++ compress.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 SITE_DIR=output
-find $SITE_DIR \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec gzip -9 -n {} \; -exec mv {}.gz {} \;
+find $SITE_DIR \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) -exec echo "gzipping" {}\; \
+    -exec gzip -9 -n {} \; \
+	-exec mv {}.gz {} \;
