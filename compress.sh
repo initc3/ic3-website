01924befa69c153b82deb2db62a922398c77d8ac
Fan Zhang <bl4ck5unxx@gmail.com>
2019-01-28 13:20:13 -0500

    improve compression script: stop compressing html

--- compress.sh
+++ compress.sh
@@ -5,7 +5,7 @@ set -e
 gzip --version
 
 SITE_DIR=output
-find $SITE_DIR \( -iname '*.html' -o -iname '*.css' -o -iname '*.js' \) \
+find $SITE_DIR \( -iname '*.css' -o -iname '*.js' \) \
 	-exec echo "gzipping" {} \; \
 	-exec gzip -9 -n {} \; \
 	-exec gzip -t {}.gz \; \
