76301c2f8bf58025b20fa671e05b84bdcecacb0c
Veronica <37443956+biblioscarlet@users.noreply.github.com>
2019-03-20 12:03:02 -0400

    Added impact

--- main.py
+++ main.py
@@ -157,6 +157,22 @@ def projects():
         challenges=data['challenges'],
         projects=data['projects']),
                        output)
+    
+      
+def impact():
+    output = join(OUTPUT_DIR, 'impact.html')
+    temp = e.env.get_template('page.html')
+
+    with codecs.open('content/impact.md', 'r', encoding='utf-8') as f:
+        content = f.read()
+        content = markdown.markdown(content)
+
+    breadcrumb = [{'name': 'Impact', 'url': 'about.html'}]
+    e.render_and_write(temp, dict(
+        title='IC3 - Impact',
+        content=content,
+        breadcrumb=breadcrumb),
+                       output)
 
 
 def publications():
@@ -309,6 +325,7 @@ if __name__ == '__main__':
     about()
     people()
     partners()
+    impact()
     projects()
     policy()
     blogs()
