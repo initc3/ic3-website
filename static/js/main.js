0aa3bd40d7b2b734f826dcdca6bdc8b813f487d0
Veronica <37443956+biblioscarlet@users.noreply.github.com>
2020-06-24 11:31:25 -0400

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@
    'microsoft': 'https://www.microsoft.com',
    'jpmorganchase': 'https://www.jpmorganchase.com/',
    'cisco': 'http://www.cisco.com/',
-   'calibra': 'http://calibra.com',
+   'novi': 'http://novi.com',
    'tendermint': 'https://tendermint.com/',
    '21co': 'https://21.co/',
    'neuralcapital': 'http://www.neuralcapital.com/',
